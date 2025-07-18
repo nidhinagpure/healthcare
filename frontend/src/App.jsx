7f668eb1894a73302a74630c868e729da014cb97
nidhinagpure <nagpurenidhi8@gmail.com>
2025-07-18 21:47:56 +0530

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
+
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Doctor from './pages/Doctor';
+import Login from './pages/Login';
+import Appointments from './pages/Appointments';
+import MyAppointment from './pages/MyAppointment';
+import MyProfile from './pages/MyProfile';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
-    <div class="text-3xl font-bold">App</div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/doctor' element={<Doctor />} />
+        <Route path='/doctor/:speciality' element={<Doctor />} />
+        <Route path='/appointments/:docId' element={<Appointments />} />
+        <Route path='/myappointment' element={<MyAppointment />} />
+        <Route path='/myprofile' element={<MyProfile />} />
+        <Route path='/contact' element={<Contact />} />
+
+      </Routes>
+     
+
+
+    </div>
   )
 }
 
