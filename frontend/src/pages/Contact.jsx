5c26b7e0327727af52c0172a79dfee265e018d3c
nidhinagpure <nagpurenidhi8@gmail.com>
2025-07-21 21:16:03 +0530

    overflow

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -13,10 +13,10 @@ const Contact = () => {
       
       <div className='flex flex-col justify-center items-start gap-6'>
         <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
-        <p className='text-gray-500'>00000 Willms Station
-          Suite 000, Washington, USA</p>
+        <p className='text-gray-500'>
+          Ganesh Nagar - Nagpur </p>
         <p className='text-gray-500'>Tel: (000) 000-0000
-          Email: greatstackdev@gmail.com</p>
+          Email:nidhinagpure21@gmail.com</p>
         <p className='font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
         <p className='text-gray-500'>Learn more about our teams and job openings.</p>
         <button className='border border-black px-8 py-4 text-sm  hover:text-white transition-all duration-500'>Explore Jobs</button>
