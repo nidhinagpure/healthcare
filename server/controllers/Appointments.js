e91be9df0d41f2177971e52deab1c543a9a6471d
nidhinagpure <nagpurenidhi8@gmail.com>
2025-08-08 00:23:06 +0530

    all changes

---
+++ server/controllers/Appointments.js
@@ -0,0 +1,261 @@
+import Appointment from "./../models/Appointment.js"
+
+
+const getAppointment = async (req, res) => {
+    const appointment = await Appointment.find();
+    return res.status(200).json({
+        success: true,
+        data: appointment,
+        message: "Appointment booked successfully",
+    });
+};
+
+const postAppointment = async (req, res) => {
+    const {
+        name,
+        sirname,
+        number,
+        email,
+        address,
+        date,
+        time,
+        day,
+        age,
+        doctorname,
+        decription
+    } = req.body; // read from req.body
+
+    /*const newBooking = {
+        name,
+        sirname,
+        number,
+        email,
+        address,
+        date,
+        time,
+        day,
+        doctorname,
+        decription ,
+    };
+     APPOINTMENT.push(newBooking); */// Object  // dont need to save in object in mongodb 
+
+    if (!name) {
+        return res.status(400).json({
+            success: false,
+            message: "name is required",
+            data: null,
+        })
+    };
+    if (!sirname) {
+        return res.status(400).json({
+            success: false,
+            message: "sirname is required",
+            data: null,
+        })
+    };
+    if (!number) {
+        return res.status(400).json({
+            success: false,
+            message: "number is required",
+            data: null,
+        })
+    };
+    if (!email) {
+        return res.status(400).json({
+            success: false,
+            message: "email is required",
+            data: null,
+        })
+    };
+    if (!address) {
+        return res.status(400).json({
+            success: false,
+            message: "address is required",
+            data: null,
+        })
+    };
+    if (!age) {
+        return res.status(400).json({
+            success: false,
+            message: "age is required",
+            data: null,
+        })
+    };
+    if (!day) {
+        return res.status(400).json({
+            success: false,
+            message: "day is required",
+            data: null,
+        })
+    }
+    if (!date) {
+        return res.status(400).json({
+            success: false,
+            message: "date is required",
+            data: null,
+        })
+    }
+    if (!time) {
+        return res.status(400).json({
+            success: false,
+            message: "time is required",
+            data: null,
+        })
+    }
+    if (!doctorname) {
+        return res.status(400).json({
+            success: false,
+            message: "doctorname is required",
+            data: null,
+        })
+    }
+    if (!decription) {
+        return res.status(400).json({
+            success: false,
+            message: "description is required",
+            data: null,
+        })
+    }
+    /* 
+    if (!name || !sirname || !number || !email || !address || !day || !date || !time || !doctorname || !decription) {
+  return res.status(400).json({
+    success: false,
+    message: "All fields are required",
+    data: null,
+  });
+}
+    */
+    const newappointment = new Appointment({
+        name,
+        sirname,
+        number,
+        email,
+        address,
+        date,
+        time,
+        age,
+        day,
+        doctorname,
+        decription,
+    });
+    const savedAppointment = await newappointment.save();
+
+    return res.status(200).json({
+        success: true,
+        data: savedAppointment,
+        message: "book appointment succesfully",
+    });
+
+};
+
+const putAppointment = async (req, res) => {
+
+    const { id } = req.params;
+    const {
+        name,
+        sirname,
+        number,
+        email,
+        address,
+        date,
+        time,
+        day,
+        age,
+        doctorname,
+        decription
+    } = req.body;
+
+    if (!/^\d{10}$/.test(number)) {
+        return res.status(400).json({
+            success: false,
+            message: "Mobile number must be exactly 10 digits",
+            data: null,
+        });
+    };
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid email address",
+            data: null,
+        });
+    }
+    try {
+
+        const appointmentUpdate = await Appointment.findById(id);
+
+        if (!appointmentUpdate) {
+            return res.status(404).json({
+                success: false,
+                message: "Appointment not found",
+                data: null,
+            });
+        }
+
+        // Update details here
+
+        appointmentUpdate.name = name;
+        appointmentUpdate.sirname = sirname;
+        appointmentUpdate.number = number;
+        appointmentUpdate.email = email;
+        appointmentUpdate.address = address;
+        appointmentUpdate.date = date;
+        appointmentUpdate.day = day;
+        appointmentUpdate.time = time;
+        appointmentUpdate.age = age;
+        appointmentUpdate.doctorname = doctorname;
+        appointmentUpdate.decription = decription;
+
+        // save update details 
+
+         appointmentUpdate.save();
+
+        return res.status(200).json({
+            success: true,
+            data: appointmentUpdate,
+            message: "Appointment details update successfully!"
+        });
+    } catch (e) {
+        return res.status(500).json({
+            success: false,
+            message: "Server error",
+            error: e.message
+        });
+    }
+};
+
+
+const deleteAppointment =  async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const appointmentDetele =  await Appointment.deleteOne({ _id: id });
+
+      if (appointmentDetele.deletedCount === 0){
+        return res.status({
+            success:false,
+            message:"Appointment Not found",
+        });
+      }
+
+      return res.status(200).json({
+        success:true,
+        message:"Appointment deleted successfully",
+      });
+
+    } catch (e) {
+            return res.status(500).json({
+                success: false,
+                message: "Server error",
+                data: e.message,
+            });
+    }
+};
+
+export { 
+    getAppointment,
+    postAppointment,
+    putAppointment,
+    deleteAppointment,
+
+ }
--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import Appointment from "./models/Appointment.js"
+import {
+    getAppointment,
+    postAppointment,
+    putAppointment,
+    deleteAppointment,
+} from "./controllers/Appointments.js";
 dotenv.config();
 
 const app = express();
-const router = express.Router();
 app.use(express.json()); // middleware to create the infomation in post api 
 app.use(cors());
 
@@ -18,259 +22,18 @@ const connectDB = async () => {
     };
 } // connection of MongoDb
 
-app.get("/bookappointment", async (req, res) => {
-    const appointment = await Appointment.find();
-    return res.status(200).json({
-        success: true,
-        data: appointment,
-        message: "Appointment booked successfully",
-    });
-});
+app.get("/bookappointment", getAppointment);
 
-app.post("/bookappointment", async (req, res) => {
-    const {
-        name,
-        sirname,
-        number,
-        email,
-        address,
-        date,
-        time,
-        day,
-        age,
-        doctorname,
-        decription
-    } = req.body; // read from req.body
+app.post("/bookappointment", postAppointment);
 
-    /*const newBooking = {
-        name,
-        sirname,
-        number,
-        email,
-        address,
-        date,
-        time,
-        day,
-        doctorname,
-        decription ,
-    };
-     APPOINTMENT.push(newBooking); */// Object  // dont need to save in object in mongodb 
+app.put("/bookappointment/:id", putAppointment);
 
-    if (!name) {
-        return res.status(400).json({
-            success: false,
-            message: "name is required",
-            data: null,
-        })
-    };
-    if (!sirname) {
-        return res.status(400).json({
-            success: false,
-            message: "sirname is required",
-            data: null,
-        })
-    };
-    if (!number) {
-        return res.status(400).json({
-            success: false,
-            message: "number is required",
-            data: null,
-        })
-    };
-    if (!email) {
-        return res.status(400).json({
-            success: false,
-            message: "email is required",
-            data: null,
-        })
-    };
-    if (!address) {
-        return res.status(400).json({
-            success: false,
-            message: "address is required",
-            data: null,
-        })
-    };
-    if (!age) {
-        return res.status(400).json({
-            success: false,
-            message: "age is required",
-            data: null,
-        })
-    };
-    if (!day) {
-        return res.status(400).json({
-            success: false,
-            message: "day is required",
-            data: null,
-        })
-    }
-    if (!date) {
-        return res.status(400).json({
-            success: false,
-            message: "date is required",
-            data: null,
-        })
-    }
-    if (!time) {
-        return res.status(400).json({
-            success: false,
-            message: "time is required",
-            data: null,
-        })
-    }
-    if (!doctorname) {
-        return res.status(400).json({
-            success: false,
-            message: "doctorname is required",
-            data: null,
-        })
-    }
-    if (!decription) {
-        return res.status(400).json({
-            success: false,
-            message: "description is required",
-            data: null,
-        })
-    }
-    /* 
-    if (!name || !sirname || !number || !email || !address || !day || !date || !time || !doctorname || !decription) {
-  return res.status(400).json({
-    success: false,
-    message: "All fields are required",
-    data: null,
-  });
-}
-    */
-    const newappointment = new Appointment({
-        name,
-        sirname,
-        number,
-        email,
-        address,
-        date,
-        time,
-        age,
-        day,
-        doctorname,
-        decription,
-    });
-    const savedAppointment = await newappointment.save();
-
-    return res.status(200).json({
-        success: true,
-        data: savedAppointment,
-        message: "book appointment succesfully",
-    });
-
-});
-
-app.put("/bookappointment/:id", async (req, res) => {
-
-    const { id } = req.params;
-    const {
-        name,
-        sirname,
-        number,
-        email,
-        address,
-        date,
-        time,
-        day,
-        age,
-        doctorname,
-        decription
-    } = req.body;
-
-    if (!/^\d{10}$/.test(number)) {
-        return res.status(400).json({
-            success: false,
-            message: "Mobile number must be exactly 10 digits",
-            data: null,
-        });
-    };
-
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-        return res.status(400).json({
-            success: false,
-            message: "Invalid email address",
-            data: null,
-        });
-    }
-    try {
-
-        const appointmentUpdate = await Appointment.findById(id);
-
-        if (!appointmentUpdate) {
-            return res.status(404).json({
-                success: false,
-                message: "Appointment not found",
-                data: null,
-            });
-        }
-
-        // Update details here
-
-        appointmentUpdate.name = name;
-        appointmentUpdate.sirname = sirname;
-        appointmentUpdate.number = number;
-        appointmentUpdate.email = email;
-        appointmentUpdate.address = address;
-        appointmentUpdate.date = date;
-        appointmentUpdate.day = day;
-        appointmentUpdate.time = time;
-        appointmentUpdate.age = age;
-        appointmentUpdate.doctorname = doctorname;
-        appointmentUpdate.decription = decription;
-
-        // save update details 
-
-         appointmentUpdate.save();
-
-        return res.status(200).json({
-            success: true,
-            data: appointmentUpdate,
-            message: "Appointment details update successfully!"
-        });
-    } catch (e) {
-        return res.status(500).json({
-            success: false,
-            message: "Server error",
-            error: e.message
-        });
-    }
-});
-
-app.delete("/bookappointment/:id", async (req, res) => {
-    const { id } = req.params;
-
-    try {
-      const appointmentDetele =  await Appointment.deleteOne({ _id: id });
-
-      if (appointmentDetele.deletedCount === 0){
-        return res.status({
-            success:false,
-            message:"Appointment Not found",
-        });
-      }
-
-      return res.status(200).json({
-        success:true,
-        message:"Appointment deleted successfully",
-      });
-
-    } catch (e) {
-            return res.status(500).json({
-                success: false,
-                message: "Server error",
-                data: e.message,
-            });
-    }
-})
+app.delete("/bookappointment/:id", deleteAppointment);
 
 app.get("/health", (req, res) => {
     res.status(200).json({ message: "server is running " })
-})
+});
+
 
 const PORT = process.env.PORT || 5003;
 
